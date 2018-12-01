ff5a877004cd526ffc7ff6672ef98babf2298516
Geng Sun <geng.sun@blocherconsulting.com>
2018-11-30 23:38:28 -0600

    works again!

--- HelloReact/ClientApp/boot.tsx
+++ HelloReact/ClientApp/boot.tsx
@@ -5,6 +5,7 @@ import * as ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import { BrowserRouter } from 'react-router-dom';
 import * as RoutesModule from './routes';
+
 let routes = RoutesModule.routes;
 
 function renderApp() {
